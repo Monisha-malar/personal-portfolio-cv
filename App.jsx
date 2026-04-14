b1f4605bee4f6dfba82402fc0b0ce5aa23a3e896
Monisha <23ad039@kpriet.ac.in>
2026-04-14 21:47:49 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="relative w-full">
+      <div className="gradient-bg"></div>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Resume />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
